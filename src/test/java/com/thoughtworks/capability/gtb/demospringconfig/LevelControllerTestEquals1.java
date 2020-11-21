f045317144e5ddaa6bceb0d1879dc0634bb9ca07
Bryan Li <yang.li@thoughtworks.com>
2020-11-21 16:20:42 +0800

    Completed the second solution by using activeprofiles annotation

--- src/test/java/com/thoughtworks/capability/gtb/demospringconfig/LevelControllerTestEquals1.java
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/LevelControllerTestEquals1.java
@@ -3,10 +3,12 @@ package com.thoughtworks.capability.gtb.demospringconfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest(properties = {"levelNumber=1"})
+@SpringBootTest
+@ActiveProfiles("test1")
 public class LevelControllerTestEquals1 {
     @Autowired
     private LevelController levelController;
